668f315cb6f4d78f9fb713c67581b41ff3a85970
lusiin <53511279+lusiin@users.noreply.github.com>
2020-09-18 15:14:43 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -38,22 +38,6 @@ const filter = new Filter();
 
 
 app.use(cors());
-app.use(fn => async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    if (req.method === 'OPTIONS') {
-      res.status(200).end()
-      return
-    }
-    return await fn(req, res)
-  });
 app.use(express.json());
 
 app.get("/", (req, res) => {
